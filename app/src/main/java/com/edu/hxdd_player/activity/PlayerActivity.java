011c3950cadb07da980c035246cc87895d8a54ba
yangx <yangx@edu-edu.com.cn>
2019-05-21 09:16:08 +0800

    添加阿里云播放demo

--- app/src/main/java/com/edu/hxdd_player/activity/PlayerActivity.java
+++ app/src/main/java/com/edu/hxdd_player/activity/PlayerActivity.java
@@ -1,4 +1,23 @@
 package com.edu.hxdd_player.activity;
 
-public class PlayerActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.aliyun.vodplayerview.activity.AliyunPlayerSkinActivity;
+import com.edu.hxdd_player.R;
+
+public class PlayerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+        findViewById(R.id.text).setOnClickListener((View v) -> {
+            // 视频播放
+            Intent intent = new Intent(PlayerActivity.this, AliyunPlayerSkinActivity.class);
+            startActivity(intent);
+        });
+    }
 }
