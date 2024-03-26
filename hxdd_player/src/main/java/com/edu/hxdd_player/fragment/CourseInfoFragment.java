aec0e859a1e9a1dd62fd68a8deb329e93dd6876e
Kangzq <kangzq@edu-edu.com.cn>
2024-03-26 15:27:48 +0800

    1.增加课程说明

--- hxdd_player/src/main/java/com/edu/hxdd_player/fragment/CourseInfoFragment.java
+++ hxdd_player/src/main/java/com/edu/hxdd_player/fragment/CourseInfoFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +12,8 @@ import androidx.fragment.app.Fragment;
 
 import com.edu.hxdd_player.R;
 import com.edu.hxdd_player.bean.CourseInfoBean;
+import com.just.agentweb.AgentWeb;
+import com.just.agentweb.DefaultWebClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +24,8 @@ import java.util.List;
 public class CourseInfoFragment extends Fragment {
     public List<CourseInfoBean.Teacher> teacherList;
     public List<CourseInfoBean.Book> textbookList;
+    AgentWeb mAgentWeb;
+    LinearLayout layout;
     public static CourseInfoFragment newInstance(ArrayList<CourseInfoBean.Teacher> teacherList, ArrayList<CourseInfoBean.Book> textbookList) {
         CourseInfoFragment fragment = new CourseInfoFragment();
         Bundle args = new Bundle();
@@ -41,9 +46,20 @@ public class CourseInfoFragment extends Fragment {
     }
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.hxdd_player_layout_recyclerview, container, false);
-
+        View view = inflater.inflate(R.layout.hxdd_player_just_webview, container, false);
+        layout =view.findViewById(R.id.layout);
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //加载网页
+        mAgentWeb = AgentWeb.with(this)
+                .setAgentWebParent(layout, new LinearLayout.LayoutParams(-1, -1))
+                .useDefaultIndicator()
+                .setOpenOtherPageWays(DefaultWebClient.OpenOtherPageWays.ASK)
+                .createAgentWeb()
+                .ready().go( "https://www.baidu.com/");
+    }
 }
