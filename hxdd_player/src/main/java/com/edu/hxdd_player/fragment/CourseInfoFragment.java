29a3598cdcff22117629dce95c11cec3f3bb3e41
Kangzq <kangzq@edu-edu.com.cn>
2025-01-13 16:39:21 +0800

    1.优化评课、纠错

--- hxdd_player/src/main/java/com/edu/hxdd_player/fragment/CourseInfoFragment.java
+++ hxdd_player/src/main/java/com/edu/hxdd_player/fragment/CourseInfoFragment.java
@@ -65,27 +65,27 @@ public class CourseInfoFragment extends Fragment {
         //加载网页
        mAgentWeb.getUrlLoader().loadUrl(url);
     }
-
-    @Override
-    public void onPause() {
-        if (mAgentWeb != null && mAgentWeb.getWebLifeCycle() != null)
-            mAgentWeb.getWebLifeCycle().onPause();
-        super.onPause();
-    }
-
-    @Override
-    public void onResume() {
-        if (mAgentWeb != null && mAgentWeb.getWebLifeCycle() != null)
-            mAgentWeb.getWebLifeCycle().onResume();
-        super.onResume();
-    }
-
-    @Override
-    public void onDestroy() {
-        if (mAgentWeb != null && mAgentWeb.getWebLifeCycle() != null)
-            mAgentWeb.getWebLifeCycle().onDestroy();
-        super.onDestroy();
-    }
+//   需要删除，否则导致评课纠错无法显示
+//    @Override
+//    public void onPause() {
+//        if (mAgentWeb != null && mAgentWeb.getWebLifeCycle() != null)
+//            mAgentWeb.getWebLifeCycle().onPause();
+//        super.onPause();
+//    }
+//
+//    @Override
+//    public void onResume() {
+//        if (mAgentWeb != null && mAgentWeb.getWebLifeCycle() != null)
+//            mAgentWeb.getWebLifeCycle().onResume();
+//        super.onResume();
+//    }
+//
+//    @Override
+//    public void onDestroy() {
+//        if (mAgentWeb != null && mAgentWeb.getWebLifeCycle() != null)
+//            mAgentWeb.getWebLifeCycle().onDestroy();
+//        super.onDestroy();
+//    }
 
     class AndroidInterface {
         public AndroidInterface(AgentWeb agentWeb, Context context) {
